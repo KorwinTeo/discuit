969e6419aaec9b5c54777e6d64f008c0473c26b0
Korwin <47688600+KorwinTeo@users.noreply.github.com>
2025-09-16 10:22:48 +0800

    Update generate-yaml.js

--- ui/scripts/generate-yaml.js
+++ ui/scripts/generate-yaml.js
@@ -1,15 +1,18 @@
 import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Get the root path
-const rootPath = path.join(import.meta.dirname, '../../');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootPath = path.join(__dirname， '../../');
 
 // Run the command to get the config
 (async () => {
   await exec('./discuit inject-config', { cwd: '../' }, async (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      console。error(`exec error: ${error}`);
       return;
     }
     if (stderr) {
@@ -18,7 +21,7 @@ const rootPath = path.join(import.meta.dirname, '../../');
     }
 
     // Make ui-config.yaml
-    await fs.writeFile(path.join(rootPath, 'ui-config.yaml'), stdout, (err) => {
+    await fs。writeFile(path.join(rootPath, 'ui-config.yaml'), stdout, (err) => {
       if (err) {
         console.error(err);
       } else {
